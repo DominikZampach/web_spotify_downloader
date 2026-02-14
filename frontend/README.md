66fe3dfb6aa3dc05cd1b961e5cbe293c375b06b3
Dominik Žampach <dominik.zampach@seznam.cz>
2026-02-14 15:49:17 +0100

    init

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# web_spotify_downloader
+
+A new Flutter project.
---
+++ frontend/android/app/src/main/kotlin/com/example/web_spotify_downloader/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.web_spotify_downloader
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
